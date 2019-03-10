14d5b691362aac874e1983b3099df8e864b038ea
Natalia Welter <nataliawelter@wp.pl>
2019-03-10 15:17:57 +0100

    add .gitignore

---
+++ src/main/java/com/fitnessblender/App.java
@@ -0,0 +1,13 @@
+package com.fitnessblender;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
