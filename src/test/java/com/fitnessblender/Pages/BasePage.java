f8c3cbf8833037462e6533618a0412f74d36d2fc
Natalia Welter <nataliawelter@wp.pl>
2019-03-11 14:32:05 +0100

    add page objects to NavbarHeader class

--- src/test/java/com/fitnessblender/Pages/BasePage.java
+++ src/test/java/com/fitnessblender/Pages/BasePage.java
@@ -7,13 +7,12 @@ import lombok.extern.log4j.Log4j2;
 @Log4j2(topic = "reporting")
 @Getter
 public abstract class BasePage {
+
     protected final static String BASE_URL = "https://www.fitnessblender.com/";
-
-
 
     public String getCurrentURL() {
         String currentURL = WebDriverRunner.url();
-        log.debug("Current page URL:" + currentURL);
+        log.debug("Current page URL: " + currentURL);
         return currentURL;
     }
 }
--- src/test/java/com/fitnessblender/Pages/commons/NavbarHeader.java
+++ src/test/java/com/fitnessblender/Pages/commons/NavbarHeader.java
@@ -9,13 +9,33 @@ import static com.codeborne.selenide.Selenide.$;
 public class NavbarHeader extends BasePage {
 
     private SelenideElement workoutsAndPrograms = $("li.workouts > a[href='/workouts-programs']");
+
+    //Workouts & Programs dropdown
+    private SelenideElement workoutVideos = $("a[href='/videos']");
+    private SelenideElement workoutPrograms = $("a[href='/plans']");
+    private SelenideElement mealPlans = $("a[href='/meal-plans']");
     private SelenideElement healthyLiving = $("li.healthy-living > a[href='/healthy-living']");
+
+    //Healthy Living dropdown
+    private SelenideElement fitness = $("a[href*='/fitness']");
+    private SelenideElement health = $("a[href*='/health']");
+    private SelenideElement healthyRecipes = $("a[href*='/healthy-recipes']");
+    private SelenideElement beforeAndAfter = $("a[href*='/before-after']");
+    private SelenideElement weightLoss = $("a[href*='/weight-loss']");
+
     private SelenideElement community = $("li.community > a[href='/community']");
     private SelenideElement blog = $("li.blog > a[href='/blog'");
     private SelenideElement about = $("li.about > a[href='/page/about-fitness-blender'");
     private SelenideElement clothing = $("li.clothing > a[href='https://workoutcomplete.com/'");
-    private SelenideElement searchIcon = $("span[data-event='click']");
     private SelenideElement cartIcon = $("a.menu-cart");
+
+    //Search box
+    private SelenideElement searchIcon = $("span[data-event='click']");
+    private SelenideElement searchField = $("#searchInput");
+    private SelenideElement searchButton = $("#searchForm button");
+
+
+
 
 
 }
