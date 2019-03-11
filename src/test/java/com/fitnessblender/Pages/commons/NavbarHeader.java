3b3847c67d910ddbcf4ebee3db2b23f617c15ef9
Natalia Welter <nataliawelter@wp.pl>
2019-03-11 15:03:49 +0100

    add search method to navbar

--- src/test/java/com/fitnessblender/Pages/commons/NavbarHeader.java
+++ src/test/java/com/fitnessblender/Pages/commons/NavbarHeader.java
@@ -3,8 +3,12 @@ package com.fitnessblender.Pages.commons;
 import com.codeborne.selenide.SelenideElement;
 import com.fitnessblender.Pages.BasePage;
 import lombok.Getter;
-import static com.codeborne.selenide.Selenide.$;
+import lombok.extern.log4j.Log4j2;
 
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Condition.*;
+
+@Log4j2(topic = "reporting")
 @Getter
 public class NavbarHeader extends BasePage {
 
@@ -35,7 +39,19 @@ public class NavbarHeader extends BasePage {
     private SelenideElement searchButton = $("#searchForm button");
 
 
-
+    public void search(String keyWords) {
+        searchIcon
+                .shouldBe(visible)
+                .click();
+        searchField
+                .shouldBe(visible)
+                .clear();
+        searchField.sendKeys(keyWords);
+        log.debug("Searching String: " + keyWords);
+        searchButton
+                .shouldBe(visible)
+                .click();
+    }
 
 
 }
