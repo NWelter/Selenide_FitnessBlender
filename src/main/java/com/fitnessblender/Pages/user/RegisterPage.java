a52d0cf98af1e157d92a8c5ef0f05c379021fd84
Natalia Welter <nataliawelter@wp.pl>
2019-03-18 23:41:52 +0100

    add validation method

--- src/main/java/com/fitnessblender/Pages/user/RegisterPage.java
+++ src/main/java/com/fitnessblender/Pages/user/RegisterPage.java
@@ -5,8 +5,11 @@ import com.fitnessblender.Pages.BasePage;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import static com.codeborne.selenide.Selenide.$;
 
@@ -23,15 +26,16 @@ public class RegisterPage extends BasePage {
     private SelenideElement passwordField = $("#password");
     private SelenideElement confirmPasswordField = $("#password_confirmation");
 
-    //TODO finish methods to fill and validation form fields
-    public RegisterPage fillRegisterForm(String firstName, String lastName, String email, String userName, String password, String confirmPassword) {
+    //TODO finish methods to fill and validation register form fields
+    public Map<SelenideElement, String> fillRegisterForm(String firstName, String lastName, String email,
+                                                         String userName, String password, String confirmPassword) {
         Map<SelenideElement, String> registerForm = new HashMap<>();
-        registerForm.put(firstNameField, firstName);
-        registerForm.put(lastNameField, lastName);
-        registerForm.put(emailField, email);
-        registerForm.put(userNameField, userName);
-        registerForm.put(passwordField, password);
-        registerForm.put(confirmPasswordField, confirmPassword);
+            registerForm.put(firstNameField, firstName);
+            registerForm.put(lastNameField, lastName);
+            registerForm.put(emailField, email);
+            registerForm.put(userNameField, userName);
+            registerForm.put(passwordField, password);
+            registerForm.put(confirmPasswordField, confirmPassword);
 
         registerForm.forEach(
                 (key, value) ->
@@ -40,7 +44,27 @@ public class RegisterPage extends BasePage {
                     key.setValue(value);
                 });
 
-        return this;
+        return registerForm;
     }
 
+    public String getRegisterFormValidationMessage(String firstName, String lastName, String email,
+                                                   String userName, String password, String confirmPassword){
+        //TODO finish method, add condition and logs
+        List<String> validationMessagesList = new ArrayList<>();
+        Map<SelenideElement, String> filledRegisterForm = fillRegisterForm(firstName, lastName, email, userName,
+                                                                            password, confirmPassword);
+        filledRegisterForm.forEach(
+                (key, value) ->
+                {
+                    if(key.parent().$("span.message").isDisplayed()) {
+                        String inputFormFieldMessage = key.parent().$("span.message").text();
+                        validationMessagesList.add(inputFormFieldMessage);
+                    }
+                });
+
+        String validationMessages = validationMessagesList.stream().collect(Collectors.joining("|"));
+        log.debug(String.format("Current validation messages from register form: [%s]", validationMessages));
+
+        return validationMessages;
+    }
 }
